a3c6d6c83b217e21a0e5452ad7e03f7f9eeff708
aspieunixgeek <asperger.unixgeek@gmail.com>
2025-06-17 00:27:33 +0500

    ns converter completed

---
+++ timeconv/ns.go
@@ -0,0 +1,18 @@
+// Nanosecond converter
+
+package timeconv
+
+// nanosecond to second
+func NsToSec(ns NanoSec) Sec {
+	return Sec(ns / 1e+9)
+}
+
+// nanosecond to millisecond
+func NsToMs(ns NanoSec) MilliSec {
+	return MilliSec(ns / 1e+6)
+}
+
+// nanosecond to micro-second
+func NsToToMicroSec(ns NanoSec) MicroSec {
+	return MicroSec(ns / 1000)
+}
---
+++ timeconv/ns_test.go
@@ -0,0 +1,42 @@
+package timeconv
+
+import (
+	"math/rand"
+	"testing"
+)
+
+// nanosecond to second
+func Test_NsToSec(t *testing.T) {
+	r := rand.Float64()
+
+	correct := Sec(r / 1e+9) // r / 1 000 000 000
+	needCheck := NsToSec(NanoSec(r))
+
+	if needCheck != correct {
+		t.Errorf("needCheck(%v) Not Equal correct(%v)", needCheck, correct)
+	}
+}
+
+// nanosecond to milliSecond
+func Test_NsToMs(t *testing.T) {
+	r := rand.Float64()
+
+	correct := MilliSec(r / 1e+6) // r / 1 000 000
+	needCheck := NsToMs(NanoSec(r))
+
+	if needCheck != correct {
+		t.Errorf("needCheck(%v) Not Equal correct(%v)", needCheck, correct)
+	}
+}
+
+// nanosecond to micro-second
+func Test_NsToMicroSec(t *testing.T) {
+	r := rand.Float64()
+
+	correct := MicroSec(r / 1000)
+	needCheck := NsToToMicroSec(NanoSec(r))
+
+	if needCheck != correct {
+		t.Errorf("needCheck(%v) Not Equal correct(%v)", needCheck, correct)
+	}
+}

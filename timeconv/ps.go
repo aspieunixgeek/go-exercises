4d9276144e645548b1a94736caba1412c30f0799
aspieunixgeek <asperger.unixgeek@gmail.com>
2025-06-17 00:14:53 +0500

    picosecond converter completed

--- timeconv/ps.go
+++ timeconv/ps.go
@@ -7,17 +7,17 @@ func PsToNs(ps PicoSec) NanoSec {
 	return NanoSec(ps / 1000)
 }
 
-// Picosecond to Microsecond
+// picosecond to microsecond
 func PsToMicSec(ps PicoSec) MicroSec {
 	return MicroSec(ps / 1e+6) // ps / 1 000 000 (million)
 }
 
-// Picosecond to Millisecond
+// picosecond to millisecond
 func PsToMs(ps PicoSec) MilliSec {
 	return MilliSec(ps / 1e+9) // ps / 1 000 000 000 (billion)
 }
 
-// Picosecond to Second
+// picosecond to second
 func PsToSec(ps PicoSec) Sec {
 	return Sec(ps / 1e+12) // ps / 1 000 000 000 000 (trillion)
 }

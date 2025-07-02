fa4a92f49398c99645c2fb2037245afcb665d802
aspieunixgeek <asperger.unixgeek@gmail.com>
2025-07-02 21:29:22 +0500

    exercise 3.5 completed

---
+++ ch3ex5/doc.go
@@ -0,0 +1,10 @@
+// Exercise 3.5:
+// Implement a full-color Mandelbrot set using the function image.NewRGBA and the type color.RGBA or color.YCbCr.
+/*
+
+# Run
+
+	$ go run ./ch3ex5.go > out.png
+
+*/
+package main

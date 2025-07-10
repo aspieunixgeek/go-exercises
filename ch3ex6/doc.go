5d076dd413af16c1df2eab766a2689a3bd9e6812
aspieunixgeek <asperger.unixgeek@gmail.com>
2025-07-10 15:34:06 +0500

    exercise 3.6 completed

---
+++ ch3ex6/doc.go
@@ -0,0 +1,13 @@
+// Exercise 3.6;
+// Supersampling is a technique to reduce the effect of
+// pixelation by computing the color value at several
+// points within each pixel and taking the average.
+// The simplest method is to divide each pixel into four "subpixels".
+// Implement it.
+
+/*
+# Run
+
+	$ go run ./ch3ex6.go > out.png
+*/
+package main
